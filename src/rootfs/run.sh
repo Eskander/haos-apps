ee53c2781e0be7f98bfe050b724eeb7d9df5ec65
Eskander Bejaoui <mail@eskander.tn>
2025-08-07 11:20:31 +0100

    Remove connectivitycheck workaround

--- src/rootfs/run.sh
+++ src/rootfs/run.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env sh
 
-CHECK=$(/get_option.sh connectivity_check)
-
-if [ -f "/www/assets/config.yml" ]; then
-    echo "Verifying config..."
-    if grep -q "^connectivityCheck:" "/www/assets/config.yml"; then
-        sed -i "s/^connectivityCheck:.*/connectivityCheck: $CHECK/" "/www/assets/config.yml"
-    else
-        echo "connectivityCheck: $CHECK" >> "/www/assets/config.yml"
-    fi
-else
-    echo "Initializing config..."
-    echo "connectivityCheck: $CHECK" >> "/www/default-assets/config.yml.dist"
-fi
-
 # Work around permission issues
 sed -i 's/^accesslog.filename.*/accesslog.filename = "\/dev\/stdout"/' /lighttpd.conf
 
